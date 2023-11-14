f05678c90ff26600d63788bc28cd0d9bdc0e2b19
akorede <emiolasaheed1@gmail.com>
2023-11-14 11:04:02 +0100

    Add name to users table

--- RecipeApp/db/schema.rb
+++ RecipeApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_14_001507) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_14_100000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_14_001507) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
