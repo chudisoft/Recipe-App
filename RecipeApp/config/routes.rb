8c9b1dd10df811172704e31d4413521c01d35cb6
Christopher Uduekwe <christopheruduekwe@gmail.com>
2023-11-15 10:55:02 +0100

    Setup food, recipes, and navigation

--- RecipeApp/config/routes.rb
+++ RecipeApp/config/routes.rb
@@ -7,4 +7,17 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
+  # Define routes for Recipes
+  resources :recipes do
+    resources :foods, only: [:new, :create], controller: 'recipe_foods' # Nested route for adding food to a recipe
+    member do
+      patch 'toggle_public', to: 'recipes#toggle_public' # Route for toggling recipe public/private status
+    end
+  end
+
+  # Define routes for Foods
+  resources :foods
+  # resources :foods, only: [:index, :new, :create, :delete]
+
 end
