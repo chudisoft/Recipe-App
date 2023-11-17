3ee57431e6f1215968cf5e272740f8cd423b5ab1
akorede <emiolasaheed1@gmail.com>
2023-11-17 17:52:38 +0100

    Validate recipe_food model

--- RecipeApp/app/models/recipe_food.rb
+++ RecipeApp/app/models/recipe_food.rb
@@ -1,4 +1,7 @@
 class RecipeFood < ApplicationRecord
   belongs_to :recipe
   belongs_to :food
+
+  validates :quantity, presence: true, numericality: { only_integer: true }, comparison: { greater_than: 0 }
+
 end
