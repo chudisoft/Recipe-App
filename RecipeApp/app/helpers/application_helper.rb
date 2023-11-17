fc87939e6a957d89691d95d211a27f3464dd0fe3
akorede <emiolasaheed1@gmail.com>
2023-11-17 10:05:25 +0100

    Create public recipe view helper methods

--- RecipeApp/app/helpers/application_helper.rb
+++ RecipeApp/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def count_public_recipe_food_item(recipe)
+    recipe.recipe_foods.count 
+  end
+  def public_recipe_food_item_price(recipe)
+    recipe.recipe_foods.reduce(0) do |sum, item|
+      sum += item.quantity * item.food.price
+    end
+  end
 end
