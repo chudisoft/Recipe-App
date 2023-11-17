d7359b9d57806a4783e79e60cf50b9815410579a
akorede <emiolasaheed1@gmail.com>
2023-11-17 17:04:08 +0100

    Test add new food button on food index page

--- RecipeApp/spec/features/food_index_spec.rb
+++ RecipeApp/spec/features/food_index_spec.rb
@@ -4,20 +4,33 @@ RSpec.describe 'Food index', type: :feature do
   Rails.application.routes.default_url_options[:host] = 'localhost:3000'
   let(:user) { User.new(name: 'example', email: 'user@example.com', password: 'password',) }
   let(:food) { Food.new(name: 'Flour', measurement_unit: 'grams', price: 2, quantity: 10, user: user) }
+ 
   before {user.save}
   before { food.save }
-  
+
+  before (:each) do
+    visit new_user_session_path
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+    visit foods_path
+  end
+
   describe 'food index' do
     it 'shows a list of foods' do
-      visit new_user_session_path
-      fill_in 'Email', with: 'user@example.com'
-      fill_in 'Password', with: 'password'
-      click_button 'Log in'
-      visit foods_path
       expect(page).to have_content('Your Foods')
       foods = user.foods.each do |food|
         expect(page).to have_content(food.name)
       end
+    end
+  end
+
+  describe 'Click events' do
+    it 'navigates to the new food page' do
+      # puts page.body
+      click_on 'Add Food'
+      expect(page).to have_current_path(new_food_path)
+      expect(page).to have_content('Add New Food')
     end
   end
 end
