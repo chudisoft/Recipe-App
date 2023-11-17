e8cab14357016eccce02d6c266016e1d581d78cc
akorede <emiolasaheed1@gmail.com>
2023-11-17 17:51:47 +0100

    Validate food model

--- RecipeApp/app/models/food.rb
+++ RecipeApp/app/models/food.rb
@@ -2,6 +2,11 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
 
+  validates :name, presence :true, length: { minimum: 1 }
+  validates :measurement_unit, presence: true
+  validates :price, presence :true, numericality: { only_integer: true }, comparison: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: { only_integer: true }, comparison: { greater_than: 0 }
+
   def food_remnant
     quantity - recipe_foods.sum(:quantity)
   end
