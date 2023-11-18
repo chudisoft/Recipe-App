cbe00a5cd0ed96bdc0eefdd28a3e1bc7e9345651
akorede <emiolasaheed1@gmail.com>
2023-11-18 09:52:15 +0100

    Write unit test for food model

--- RecipeApp/app/models/food.rb
+++ RecipeApp/app/models/food.rb
@@ -2,9 +2,9 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
 
-  validates :name, presence :true, length: { minimum: 1 }
+  validates :name, presence: true, length: { minimum: 1 }
   validates :measurement_unit, presence: true
-  validates :price, presence :true, numericality: { only_integer: true }, comparison: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
   validates :quantity, presence: true, numericality: { only_integer: true }, comparison: { greater_than: 0 }
 
   def food_remnant
